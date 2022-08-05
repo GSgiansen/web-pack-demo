f96dc8db361c52c98e03bb88dd7fb575a7439ae8
GianSen <giansenrockz@gmail.com>
2022-08-06 00:30:35 +0800

    Initialised repository

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import myName from './myName';
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  // use your function!
+  element.textContent = myName('Cody');
+  element.classList.add("hello")
+  return element;
+}
+
+document.body.appendChild(component());
